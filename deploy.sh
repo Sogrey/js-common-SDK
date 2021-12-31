5ccdc5de5764fa46a22244dd9c9d94a167acadf2
sogrey <suzhaoheng2006@163.com>
2021-12-31 23:02:39 +0800

    deploy main

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+echo -e '\033[32;40m
+
+  ____        U  ___ u    ____      ____     U _____ u   __   __
+ / __"| u      \/"_ \/ U /"___|u U |  _"\ u  \| ___"|/   \ \ / /
+<\___ \/       | | | | \| |  _ /  \| |_) |/   |  _|"      \ V /
+ u___) |   .-,_| |_| |  | |_| |    |  _ <     | |___     U_|"|_u
+ |____/>>   \_)-\___/    \____|    |_| \_\    |_____|      |_|
+  )(  (__)       \\\\      _)(|_     //   \\\\_   <<   >>  .-,//|(_
+ (__) .github.io(__)    (__)__)   (__)  (__) (__) (__)  \_) (__)  
+ 
+'
+
+echo -e "\033[32;40m [1/2] \033[0m run release"
+
+yarn release
+
+echo -e "\033[32;40m [2/2] \033[0m commit 2 main branch"
+
+git init
+git add -A
+git commit -m 'deploy main'
+git push -f git@github.com:Sogrey/js-common-SDK.git main
+
+echo -e "\033[32;40m done \033[0m "
